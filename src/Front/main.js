7481e840c91a65b6aaf49293ae2979268574006f
ElayGelbart <elaygelbart@gmail.com>
2021-11-07 18:03:55 +0200

    Frontend - Add Person to phone book

--- src/Front/main.js
+++ src/Front/main.js
@@ -17,4 +17,54 @@ const getPhoneBook = async () => {
   }
 }
 getPhoneBook();
+
+const addPersonToServer = async () => {
+  const userNameValue = document.getElementById("validationCustomName").value;
+  const userPhoneNumberValue = document.getElementById("validationCustomPhoneNumber").value;
+  if (!validator.isAlpha(userNameValue) || !validator.isLength(userNameValue, { min: 3, max: 15 })) {
+    document.getElementById("validationCustomName").classList.add("is-invalid")
+    document.getElementById("nameValidationText").innerText = "Name Not Valid";
+    document.getElementById("nameValidationText").className = "invalid-feedback";
+    return;
+  } else {
+    document.getElementById("validationCustomName").classList.remove("is-invalid")
+    document.getElementById("validationCustomName").classList.add("is-valid")
+    document.getElementById("nameValidationText").innerText = "Good Name";
+    document.getElementById("nameValidationText").className = "valid-feedback";
+  }
+  if (!validator.isMobilePhone(userPhoneNumberValue)) {
+    document.getElementById("validationCustomPhoneNumber").classList.add("is-invalid")
+    document.getElementById("phoneValidationText").innerText = "Phone Not Valid";
+    document.getElementById("phoneValidationText").className = "invalid-feedback";
+    return;
+  } else {
+    document.getElementById("validationCustomPhoneNumber").classList.remove("is-invalid")
+    document.getElementById("validationCustomPhoneNumber").classList.add("is-valid")
+    document.getElementById("phoneValidationText").innerText = "Great Phone";
+    document.getElementById("phoneValidationText").className = "valid-feedback";
+  }
+  try {
+    const response = await fetch(`${HOST}/api/persons`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: userNameValue,
+        number: userPhoneNumberValue
+      })
+    }).then((res) => {
+      if (!res.ok) {
+        throw new Error("UserNameTaken")
+      }
+    });
+    console.log("here");
+    alert("Added to phone book");
+  } catch (err) {
+    document.getElementById("validationCustomName").classList.add("is-invalid")
+    document.getElementById("nameValidationText").innerText = "Name Taken";
+    document.getElementById("nameValidationText").className = "invalid-feedback";
+  }
+}
 document.getElementById("getPhonebookBtn").addEventListener("click", getPhoneBook)
+document.getElementById("sendServerNewPerson").addEventListener("click", addPersonToServer)

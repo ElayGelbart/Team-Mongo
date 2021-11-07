d44c6009444d111ebb81ddd7febf3e31d92a8b88
EhoodGoren <ehoodgoren@gmail.com>
2021-11-07 21:53:53 +0200

    3.5 Server now has ability to post a new phonebook entry.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 const data = require('./data');
 
-
+app.use(express.json());
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 })
@@ -14,7 +14,7 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    const phonebookEntries = Object.keys(data).length;
+    const phonebookEntries = data.length;
     res.send(`Phonebook has info for ${phonebookEntries} people. \n ${Date()}`);
 })
 
@@ -32,6 +32,14 @@ app.delete('/api/persons/:id', (req, res) => {
     matchingPerson
     ? data.splice(data.indexOf(matchingPerson),1)
     : res.status(204).send({error: "Can't find person"})
+    res.send();
+})
+
+app.post('/api/persons', (req, res) => {
+    const newId = Math.floor(Math.random()*1000);
+    const { name, number } = req.body
+    const newPerson = {id: newId, name, number };
+    data.push(newPerson);
     res.send();
 })
 

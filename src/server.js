0f96955283249c3bd4da278d3c213f02c280b947
shakederin <89574610+shakederin@users.noreply.github.com>
2021-11-07 18:29:15 +0200

    3.3

--- src/server.js
+++ src/server.js
@@ -6,12 +6,25 @@ const PORT = 3001;
 
 app.listen(PORT, ()=>{
     console.log("running...");
-})
+});
 
 app.get("/api/persons",  function (req,res){
-    res.send(data);
-})
+    res.send(data.data);
+});
 
 app.get("/info", function (req,res){
     res.send({Contacts: data.data.length, date:new Date()})
+});
+
+app.get("/api/persons/:id", function (req,res){
+    const id = req.params.id;
+    let contact;
+    for (let i =0; i<data.data.length; i++){
+        if(+data.data[i].id === +id){
+            contact = data.data[i];
+            res.send(contact);
+            return
+        }
+    }
+    res.sendStatus(404);
 })

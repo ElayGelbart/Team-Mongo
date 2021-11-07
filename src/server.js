b3dfaa51afb339ca13c9f78bb4fb1a264f62b5ca
shakederin <89574610+shakederin@users.noreply.github.com>
2021-11-07 17:46:30 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
 // server code
+const express = require('express');
+const app = express();
+const data = require("./database")
+const PORT = 3001;
+
+app.listen(PORT, ()=>{
+    console.log("running...");
+})
+
+app.get("/api/persons",  function (req,res){
+    res.send(data);
+})
+

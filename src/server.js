bb44c7fac38ad40c8ff38676f34729bcadbdbb80
Eithan Refaely <eithanrefaely@gmail.com>
2021-11-08 00:46:00 +0200

    Finished 3.1
    Finished 3.1 and initial commit and publish for my development branch.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-// server code
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { data } = require("../data")
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true} ))
+app.use('/', express.static(path.resolve("./front")));               // serve main path as static dir
+
+
+app.get('/', async (req, res) => {     
+    res.sendFile(path.resolve("../front/index.html"))
+  });
+app.get("/api/persons", (req, res) => {
+    res.send(data)
+})
+
+app.listen(process.env.PORT || 3001);

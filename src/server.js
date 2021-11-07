ec604a2de4ff5537ddcfda0bd0b7e0b6214fae33
Eithan Refaely <eithanrefaely@gmail.com>
2021-11-08 00:57:08 +0200

    Finished 3.2

--- src/server.js
+++ src/server.js
@@ -13,8 +13,17 @@ app.use('/', express.static(path.resolve("./front")));               // serve ma
 app.get('/', async (req, res) => {     
     res.sendFile(path.resolve("../front/index.html"))
   });
+
 app.get("/api/persons", (req, res) => {
     res.send(data)
 })
+
+app.get("/info", (req, res) => {
+    res.send(`<h1>PhoneBook has info for ${data.length} Pepole</h1>
+                <p>LastSync: ${new Date().toLocaleString()} </p>`)
+})
+
+
+
 
 app.listen(process.env.PORT || 3001);

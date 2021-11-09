1be182d4bc7fb57534eff226fe9945d051b7123f
ElayGelbart <elaygelbart@gmail.com>
2021-11-09 15:40:05 +0200

    MVC

--- src/Back/Models/Person.js
+++ src/Back/Models/Person.js
@@ -2,13 +2,9 @@ const mongoose = require("mongoose");
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
-    maxLength: 20
   },
   phoneNumber: {
     type: String,
-    minLength: 10,
-    maxLength: 40
   }
 });
 module.exports = mongoose.model('Person', personSchema);

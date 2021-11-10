d73060e470b7732e9c24dcf8d2f7632256bf53d8
ElayGelbart <elaygelbart@gmail.com>
2021-11-10 08:40:38 +0200

    Mongo Validation

--- src/Back/Models/Person.js
+++ src/Back/Models/Person.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 const personSchema = new mongoose.Schema(
   {
-    name: String,
-    phoneNumber: String
+    name: {
+      type: String,
+      minlength: 3,
+      maxlength: 20
+    },
+    phoneNumber: {
+      type: String,
+      minlength: 7,
+      maxlength: 20
+    }
   }
 );
 module.exports = mongoose.model('Person', personSchema);
